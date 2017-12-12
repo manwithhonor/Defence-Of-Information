9616e0d23f07c1716c51a40fcf8d2fccd05d5f33
Sharapov_Roman <sharapov.roman@gmail.com>
2017-12-12 16:49:24 +0300

    working

--- Password_utility/Cipher.h
+++ Password_utility/Cipher.h
@@ -60,17 +60,8 @@ using std::vector;
 using std::numeric_limits;
 
 
-typedef unsigned long long AES_DATA_128[ 2 ];
-typedef unsigned long long AES_KEY_128[ 2 ];
-typedef unsigned long long AES_KEY_256[ 4 ];
-typedef unsigned long long AES_ROUND_KEYS_128_128[ 11 * 2 ];
+
 typedef unsigned long long AES_ROUND_KEYS_128_256[ 15 * 2 ];
-
-void Test_AES_128_128_Speed_Expanded_Memory();
-
-void Test_AES_128_256_Speed_Expanded_Memory();
-
-void print_aes_test();
 
 
 typedef unsigned int u32;
